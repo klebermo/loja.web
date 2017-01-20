c65fd92c2add918bdd26a3399bab61255519c259
Kleber Mota <klebermo@gmail.com>
2017-01-20 18:20:10 -0300

    Controller.java

--- src/main/java/org/kleber/loja/model/Controller.java
+++ src/main/java/org/kleber/loja/model/Controller.java
@@ -6,4 +6,36 @@ public abstract class Controller {
   public Controller(Class clazz) {
     this.clazz = clazz;
   }
+
+  public void insert() {
+    //
+  }
+
+  public String insert() {
+    return "form/insert";
+  }
+
+  public void update() {
+    //
+  }
+
+  public String update() {
+    return "form/update";
+  }
+
+  public void delete() {
+    //
+  }
+
+  public String delete() {
+    return "form/delete";
+  }
+
+  public String select(String orderby, String groupby) {
+    return null;
+  }
+
+  public String select() {
+    return "home/"+clazz.getSimpleName();
+  }
 }
