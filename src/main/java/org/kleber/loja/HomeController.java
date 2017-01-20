c20cfb898832ac32bd3a0df7b1cd88f8a39173f5
Kleber Mota <klebermo@gmail.com>
2017-01-19 22:29:44 -0300

    HomeController.java

--- src/main/java/org/kleber/loja/HomeController.java
+++ src/main/java/org/kleber/loja/HomeController.java
@@ -8,9 +8,44 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    @RequestMapping(value = "/")
+    public String index() {
+      return "index";
+    }
+
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "home";
+    }
+
+    @RequestMapping(value = "/categoria")
+    public String categoria() {
+      return "categoria";
+    }
+
+    @RequestMapping(value = "/produto")
+    public String produto() {
+      return "produto";
+    }
+
+    @RequestMapping(value = "/pagina")
+    public String pagina() {
+      return "pagina";
+    }
+
+    @RequestMapping(value = "/cart")
+    public String cart() {
+      return "cart";
+    }
+
+    @RequestMapping(value = "/order")
+    public String order() {
+      return "order";
+    }
+
+    @RequestMapping(value = "/wishlist")
+    public String wishlist() {
+      return "wishlist";
     }
 }
